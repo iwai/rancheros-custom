9beac99f4cc2cbc0aca6bc5e081b8c74bfe813c7
Yuji Iwai <iwai.ug@gmail.com>
2019-06-28 07:48:56 +0900

    google-authenticator-libpamのビルド成功
    - 必要なPAMライブラリを追加
    - gitがうまくダウンロードできない問題の修正

--- package/google-authenticator-libpam/google-authenticator-libpam.mk
+++ package/google-authenticator-libpam/google-authenticator-libpam.mk
@@ -3,9 +3,10 @@
 # google-authenticator-libpam
 #
 ################################################################################
+
 GOOGLE_AUTHENTICATOR_LIBPAM_VERSION = 1.06
-GOOGLE_AUTHENTICATOR_LIBPAM_SOURCE = $(GOOGLE_AUTHENTICATOR_LIBPAM_VERSION).tar.gz
-GOOGLE_AUTHENTICATOR_LIBPAM_SITE = https://github.com/google/google-authenticator-libpam/archive/$(GOOGLE_AUTHENTICATOR_LIBPAM_VERSION).tar.gz
+GOOGLE_AUTHENTICATOR_LIBPAM_SITE = https://github.com/google/google-authenticator-libpam
+GOOGLE_AUTHENTICATOR_LIBPAM_SITE_METHOD = git
 
 GOOGLE_AUTHENTICATOR_LIBPAM_AUTORECONF = YES
 GOOGLE_AUTHENTICATOR_LIBPAM_AUTORECONF_OPTS = -i
